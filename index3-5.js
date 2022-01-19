3dcace614963a22508e465cf6c83ed7a557802b9
beck1078398 <beck1078398@gmail.com>
2022-01-19 13:08:52 +0800

    v3.6

---
+++ index3-5.js
@@ -0,0 +1,7 @@
+let n = 0;
+while(n <3) {
+    console.log('迴圈內過程:',n)
+    n++;
+}
+
+console.log('迴圈外結果:',n);
---
+++ index3-6.js
@@ -0,0 +1,12 @@
+let card ={
+    name: '王大明',
+    tel: '0800-000-000',
+    addr: '中央市中央街1號',
+    '職稱':'工程師'
+};
+
+
+
+for(let i in card){
+    console.log(i,":",card[i]);
+}
