2323675c34beec1661b23097443af3f36491cc0c
beck1078398 <beck1078398@gmail.com>
2022-01-19 10:58:44 +0800

    v3.4

--- index3-1.js
+++ index3-1.js
@@ -1,4 +1,21 @@
-if(score >= 90){
-    console.log("您的分數是A級")
+let score = 55
 
+if(score >= 90){
+    console.log("您的分數是A級");
+}else if(score >=80){
+    console.log("您的分數是B級");
+}else if(score >=70){
+    console.log("您的分數是C級");
+}else if(score >=60){
+    console.log("您的分數是D級");
+}else{
+    console.log("您沒通過考試，請下次再來")
 }
+
+// let a = '';
+// a += '1,/n';
+// a = a+ '1,';
+// a +='2'
+let a = `1,
+2`
+console.log(a)
--- index3-2.js
+++ index3-2.js
@@ -1,13 +1,25 @@
-const expr = 'Papayas';
-switch (expr) {
+const EXPR = 'Mangoes';
+switch (EXPR) {
   case 'Oranges':
-    console.log('Oranges are $0.59 a pound.');
+    Oranges();
     break;
   case 'Mangoes':
   case 'Papayas':
-    console.log('Mangoes and papayas are $2.79 a pound.');
+    Papayas();
     // expected output: "Mangoes and papayas are $2.79 a pound."
     break;
   default:
-    console.log(`Sorry, we are out of ${expr}.`);
+    Default();
+}
+
+function Oranges(){
+    console.log('Oranges are $0.59 a pound.');
+}
+
+function Papayas(){
+    console.log('Mangoes and papayas are $2.79 a pound.');
+}
+
+function Default(){
+    console.log(`Sorry, we are out of ${EXPR}.`);
 }
